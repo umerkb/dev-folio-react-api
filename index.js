78180ab9465a5246063c0857f7a1068024ffff4f
Umer Khalid Butt <UKButt@dartican.com>
2022-05-11 19:03:09 +0500

    setting up routes for all modules

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const express = require("express");
 const app = express();
-const menuRouter = require("./routes/menu");
 const loggers = require("./middlewares/loggers");
+const menuRouter = require("./routes/menu");
+const homeRouter = require("./routes/home");
+const aboutRouter = require("./routes/about");
+const servicesRouter = require("./routes/services");
+const workRouter = require("./routes/work");
+const contactRouter = require("./routes/contact");
 
 app.use(express.json());
 app.use(loggers.requestLogger);
+
 app.use("/api/menus", menuRouter);
+app.use("/api/home", homeRouter);
+app.use("/api/about", aboutRouter);
+app.use("/api/services", servicesRouter);
+app.use("/api/contact", contactRouter);
+app.use("/api/work", workRouter);
 
 app.listen(5000, () => {
   console.log("Listening on port 5000");
